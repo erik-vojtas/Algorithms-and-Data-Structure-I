92b38ed5afbd4b12b881dbe83d1908b83237fdb3
erik-vojtas <61457905+erik-vojtas@users.noreply.github.com>
2020-06-22 17:52:10 +0200

    Add files via upload

--- MergeSortAlgorithm.py
+++ MergeSortAlgorithm.py
@@ -3,40 +3,52 @@
 # split an array into two halves, sort these two halves and then merge them together in an order
 
 comparisons = 0
-iteration = 0
-swaps = 0
-def MergeSort(array, start, end):
-    global iteration
-    global swaps
+recursions = 0
+
+# decorator to count recursion
+def countRecursion(fn):
+    def wrappedFunction(*args):
+        global recursions
+        if fn(*args):
+            recursions += 1
+    return wrappedFunction
+
+@countRecursion
+def mergeSort(list):
     global comparisons
-    if start < end:
-        pIndex = Partition(array, start, end)
-        MergeSort(array, start, pIndex - 1)
-        MergeSort(array, pIndex + 1, end)
-    return f"Sorted array: {array}, comparisons: {comparisons}, swaps {swaps}"
+    global recursions
+    if len(list) > 1:
+        mid = len(list) // 2
+        left_list = list[:mid]
+        right_list = list[mid:]
+        mergeSort(left_list)    # recursion
+        mergeSort(right_list)   # recursion
+        i = j = k = 0
 
 
-def Partition(array, start, end):
-    global comparisons
-    global iteration
-    global swaps
-    pivot = array[end]
-    pIndex = start
-    for i in range(start, end - 1):
-        comparisons += 1
-        if array[i] <= pivot:
-            array[i], array[pIndex] = array[pIndex], array[i]
-            swaps += 1
-            pIndex += 1
-    array[pIndex], array[end] = array[end], array[pIndex]
-    swaps += 1
-    comparisons += 1
-    iteration += 1
-    print(f"Iteration {iteration}: {array}")
-    return pIndex
+        while i < len(left_list) and j < len(right_list):
+            comparisons += 1
+            if left_list[i] < right_list[j]:
+                list[k] = left_list[i]
+                i += 1
+            else:
+                list[k] = right_list[j]
+                j += 1
+            k += 1
+
+        while i < len(left_list):
+            list[k] = left_list[i]
+            i += 1
+            k += 1
+
+        while j < len(right_list):
+            list[k] = right_list[j]
+            j += 1
+            k += 1
+    return True
 
 
-#array = [2, 7, 4, 1, 5, 3]
 array = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
-print(f"Unsorted array: {array}")
-print(MergeSort(array, 0, len(array)-1))
+print(f"Unsorted array: {array}.")
+mergeSort(array)
+print(f"Sorted array: {array}, comparisons: {comparisons}, recursions: {recursions}.")
