0a7367530bb053c0f34147fc75ca1d2f0a416460
erik-vojtas <61457905+erik-vojtas@users.noreply.github.com>
2020-06-22 16:25:47 +0200

    Add files via upload

--- QuickSortAlgorithm.py
+++ QuickSortAlgorithm.py
@@ -1,6 +1,15 @@
 # Quick Sort Algorithm
 # https://www.programiz.com/dsa/quick-sort
-# Set first item in an array as sorted
+# 1. A pivot element is chosen from the array. You can choose any element from the array as the pivot element. Here, we have taken the rightmost (ie. the last element) of the array as the pivot element
+# 2. The elements smaller than the pivot element are put on the left and the elements greater than the pivot element are put on the right.
+# The above arrangement is achieved by the following steps.
+# a) A pointer is fixed at the pivot element. The pivot element is compared with the elements beginning from the first index. If the element greater than the pivot element is reached, a second pointer is set for that element.
+# b) Now, the pivot element is compared with the other elements (a third pointer). If an element smaller than the pivot element is reached, the smaller element is swapped with the greater element found earlier.
+# c) The process goes on until the second last element is reached. Finally, the pivot element is swapped with the second pointer.
+# 3. Pivot elements are again chosen for the left and the right sub-parts separately. Within these sub-parts, the pivot elements are placed at their right position. Then, step 2 is repeated.
+# 4. The sub-parts are again divided into smaller sub-parts until each subpart is formed of a single element.
+# 5. At this point, the array is already sorted.
+
 
 comparisons = 0
 iteration = 0
@@ -23,7 +32,7 @@ def Partition(array, start, end):
     global swaps
     pivot = array[end]
     pIndex = start
-    for i in range(start, end - 1):
+    for i in range(start, end):
         comparisons += 1
         if array[i] <= pivot:
             array[i], array[pIndex] = array[pIndex], array[i]
