7600c8546f6a11a3f8facc9cc57eab0a83485d81
erik-vojtas <61457905+erik-vojtas@users.noreply.github.com>
2020-06-25 17:43:12 +0200

    Add files via upload

---
+++ BinaryTree.py
@@ -0,0 +1,41 @@
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data)
+        if self.right:
+            self.right.printTree()
+
+
+rootNode = Node(5)
+# rootNode.printTree()
+
+rootNode.insert(4)
+rootNode.insert(3)
+rootNode.insert(7)
+rootNode.insert(6)
+rootNode.printTree()
+print("----------------------------")
+
+print(rootNode.data)
