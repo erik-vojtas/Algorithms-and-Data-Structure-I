ad4a29d68938a47e3f8bd2d14e62746331f60783
erik-vojtas <61457905+erik-vojtas@users.noreply.github.com>
2020-06-29 09:59:24 +0200

    Add files via upload

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,31 +1,33 @@
+# Binary Tree
+# https://www.programiz.com/dsa/binary-tree
 
 class Node:
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
+    def __init__(self, data): # initiate Node
+        self.left = None # left pointer
+        self.right = None # right pointer
+        self.data = data # data part
 
-    def insert(self, data):
-        if self.data:
-            if data < self.data:
-                if self.left is None:
-                    self.left = Node(data)
+    def insert(self, data): # insert data into binary tree
+        if self.data: # if self.data exists
+            if data < self.data: # if inserted data < self.data
+                if self.left is None: # if left pointer is None
+                    self.left = Node(data) # then Node(inserted data) is assigned to left pointer
+                else: # else
+                    self.left.insert(data) # recursion
+            elif data > self.data: # if inserted data > self.data
+                if self.right is None: # if right pointer is None
+                    self.right = Node(data) # then Node(inserted data) is assigned to right pointer
                 else:
-                    self.left.insert(data)
-            elif data > self.data:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.right.insert(data)
-        else:
-            self.data = data
+                    self.right.insert(data) # recursion
+        else: # if self.data does not exist
+            self.data = data # self.data = inserted data
 
-    def printTree(self):
-        if self.left:
-            self.left.printTree()
-        print(self.data)
-        if self.right:
-            self.right.printTree()
+    def printTree(self): # print Binary Tree
+        if self.left: # if left pointer exists
+            self.left.printTree() # recursion
+        print(self.data) # print data
+        if self.right: # if right pointer exists
+            self.right.printTree() # recursion
 
 
 rootNode = Node(5)
